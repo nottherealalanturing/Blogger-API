d100967b9c37768442144f68c14f7ef220e6adfe
Assad Isah <nottherealalanturing@gmail.com>
2023-03-18 23:20:58 +0100

    update README.md

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
+
 const MONGODB_URI = () => {
-  if (process.env.NODE_ENV === "production")
-    return process.env.PROD_MONGODB_URI;
-  else if (process.env.NODE_ENV === "development")
-    return process.env.DEV_MONGODB_URI;
-  else return process.env.TEST_MONGODB_URI;
+  return process.env.MONGODB_URI;
 };
 
 module.exports = {
