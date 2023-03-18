96438d9758d9a6858f20485b40cddfaa93d72a18
Assad Isah <nottherealalanturing@gmail.com>
2023-03-19 00:08:05 +0100

    fix bug in errorhandler middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -14,24 +14,20 @@ const unknownEndpoint = (request, response) => {
 };
 
 const errorHandler = (error, request, response, next) => {
-  //logger.error(error.message);
-
+  logger.error(error.message);
   if (error.name === "CastError")
     response.status(404).json({ error: "malformatted id" });
   else if (error.name === "ValidationError")
     response.status(404).json({ error: error.message });
   else if (error.name === "JsonWebTokenError")
     return response.status(400).json({ error: error.message });
-  else if (err.code === 11000)
-    return response.status(400).json({ error: error.message });
   else if (error.name === "TokenExpiredError")
     return response.status(401).json({
       error: "token expired",
     });
-  else if (err.code === 11000) {
+  else if (error.code === 11000) {
     res.status(400).send({ message: "Email already exists" });
   } else {
-    //console.error(err);
     res.status(500).send({ message: "Internal server error" });
   }
 
